29acdc23b266bb261624c1b0b1478e70da2d03dd
PCloud <pcloud63514@gmail.com>
2022-04-20 11:09:13 +0900

    remove web page file

--- applications/web-next-ohlot/components/Title.tsx
+++
@@ -1,7 +0,0 @@
-import Head from "next/head";
-
-export default function Title({title}:any) {
-    return <Head>
-        <title>{title} | Next Movies </title>
-    </Head>
-}
--- applications/web-ohlot/src/pages/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as GoodWordPage } from './goodWordPage/GoodWordPage'
