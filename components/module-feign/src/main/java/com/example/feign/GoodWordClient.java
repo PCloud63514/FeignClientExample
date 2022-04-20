e4d2fccbee6cd56bd5adcb426cf0ea30ed376506
PCloud <pcloud63514@gmail.com>
2022-04-20 11:08:32 +0900

    Update

--- components/module-feign/src/main/java/com/example/feign/GoodWordClient.java
+++ components/module-feign/src/main/java/com/example/feign/GoodWordClient.java
@@ -1,6 +1,7 @@
 package com.example.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -10,18 +11,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.awt.*;
 import java.util.List;
 import java.util.UUID;
 
 @FeignClient(name = "good-word", url = "localhost:8888/good-words")
 public interface GoodWordClient {
-    @PostMapping
-    void addGoodWord(@Valid @RequestBody GoodWordAddRequest request);
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    GoodWordAddResponse addGoodWord(@Valid @RequestBody GoodWordAddRequest request);
 
     @GetMapping
     List<GoodWordGetResponse> getGoodWords();
 
-    @PatchMapping("{id}")
+    @PatchMapping(value = "{id}")
     void updateGoodWord(@PathVariable UUID id, @RequestParam String content);
 
     @DeleteMapping("{id}")
--- components/module-feign/src/test/java/com/example/feign/GoodWordClientTest.java
+++ components/module-feign/src/test/java/com/example/feign/GoodWordClientTest.java
@@ -6,13 +6,15 @@ import feign.Feign;
 import feign.Logger;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
+import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GoodWordClientTest {
     GoodWordClient goodWordClient;
@@ -20,6 +22,7 @@ class GoodWordClientTest {
     @BeforeEach
     void setUp() {
         goodWordClient = Feign.builder()
+                .client(new OkHttpClient())
                 .decoder(new JacksonDecoder(new WebConfig().objectMapper(new JsonMapperJava8DateTimeModule())))
                 .encoder(new JacksonEncoder())
                 .contract(new SpringMvcContract())
@@ -32,6 +35,31 @@ class GoodWordClientTest {
     void getGoodWords_callTest() {
         List<GoodWordGetResponse> goodWords = goodWordClient.getGoodWords();
 
-        Assertions.assertThat(goodWords).isNotNull();
+        assertThat(goodWords).isNotNull();
+    }
+
+    @Test
+    void addGoodWord_callTest() {
+        String givenContent = "안녕하세요";
+
+        GoodWordAddResponse response = goodWordClient.addGoodWord(new GoodWordAddRequest(givenContent));
+
+        assertThat(response).isNotNull();
+        assertThat(response.getContent()).isEqualTo(givenContent);
+    }
+
+    @Test
+    void updateGoodWord_callTest() {
+        String givenContent = "반가워요";
+        GoodWordAddResponse givenResponse = goodWordClient.addGoodWord(new GoodWordAddRequest("안녕하세요"));
+
+        goodWordClient.updateGoodWord(givenResponse.getId(), givenContent);
+    }
+
+    @Test
+    void deleteGoodWord_callTest() {
+        GoodWordAddResponse givenResponse = goodWordClient.addGoodWord(new GoodWordAddRequest("장난이에요"));
+
+        goodWordClient.deleteGoodWord(givenResponse.getId());
     }
 }
